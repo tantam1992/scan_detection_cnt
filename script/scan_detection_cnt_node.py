e3aace7f2df3e93ec7a35b090a45673649c235a1
JosefGst <josefgstoettner@live.at>
2023-12-05 10:41:35 +0800

    minor updates

--- script/scan_detection_cnt_node.py
+++ script/scan_detection_cnt_node.py
@@ -4,7 +4,8 @@ from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Int16
 import math
 
-rospy.init_node('laser_sub', anonymous=True)
+rospy.init_node('scan_detection_cnt', anonymous=True)
+rospy.loginfo("scan detection counter started")
 pub = rospy.Publisher('detections', Int16, queue_size=10)
 
 # Counts the number of valid range values in a LaserScan message.
@@ -14,14 +15,13 @@ def callback(data):
     for i in range(len(data.ranges)):
         # check if value is not nan
         if not math.isnan(data.ranges[i]) and data.ranges[i] != 0.0:
-            rospy.loginfo("valid")
             count += 1
     # rospy.loginfo("Number of obstacle detected: %d", count)
     pub.publish(count)   
     
-def laser_sub():
+def scan_detection_cnt():
     rospy.Subscriber("scan_filtered", LaserScan, callback)
     rospy.spin()
 
 if __name__ == '__main__':
-    laser_sub()
+    scan_detection_cnt()
